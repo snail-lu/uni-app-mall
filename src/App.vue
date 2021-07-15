51e1c3950f54f7519794b546f68888afee6b4632
snaillu <snaillu@yeah.net>
2021-07-15 15:52:43 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.warn(
+			"当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！"
+		);
+		console.log("App Launch");
+	},
+	onShow: function() {
+		console.log("App Show");
+	},
+	onHide: function() {
+		console.log("App Hide");
+	}
+};
+</script>
+
+<style>
+/*页面公共css */
+page {
+	min-height: 100%;
+	background-color: #f5f5f5;
+}
+.flex-box {
+	display: flex;
+}
+.flex-h-between {
+	justify-content: space-between;
+}
+.flex-h-center {
+	justify-content: center;
+}
+.flex-h-end {
+	justify-content: flex-end;
+}
+.flex-box-v {
+	display: flex;
+	flex-direction: column;
+}
+.flex-v-center {
+	align-items: center;
+}
+.flex-v-end {
+	align-items: flex-end;
+}
+
+.flex-wrap {
+	flex-wrap: wrap;
+}
+.flex-item-1 {
+	flex: 1;
+}
+.flex-item-2 {
+	flex: 2;
+}
+.flex-item-3 {
+	flex: 3;
+}
+.flex-item-4 {
+	flex: 4;
+}
+.flex-item-5 {
+	flex: 5;
+}
+.flex-shrink-0 {
+	flex-shrink: 0;
+}
+.flex-box-column {
+	display: flex;
+	flex-direction: column;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './App';
+import store from './store';
+import commonMixins from '@/common/mixins/commonMixins';
+
+Vue.config.productionTip = false;
+Vue.prototype.$store = store;
+Vue.mixin(commonMixins);
+
+App.mpType = 'app';
+
+const app = new Vue({
+	store,
+	...App
+});
+app.$mount();
