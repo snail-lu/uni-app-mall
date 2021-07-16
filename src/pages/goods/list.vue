df8f5e557543b0f520e3fa2f120ca6ca3b1f36e5
snaillu <snaillu@yeah.net>
2021-07-16 16:57:34 +0800

    商品列表页简单实现

--- src/pages/goods/list.vue
+++ src/pages/goods/list.vue
@@ -1,17 +1,189 @@
 <template>
-	<view>
-		<button type="default" @click="navigateTo">商品详情页</button>
+	<view class="goodslist-container">
+		<!-- 排序栏 -->
+		<view class="sort-box flex-box flex-h-between">
+			<view
+				class="sort-item flex-item-1 flex-box flex-v-center flex-h-center"
+				v-for="item in sortList"
+				:key="item.prop"
+				@click="changeSortField(item.prop, fieldValue)"
+			>
+				<text :class="{ 'item-active': currentField == item.prop }">{{ item.label }}</text>
+				<block v-if="currentField == item.prop">
+					<view :class="{ 'triangle-up-active': fieldValue == 2 }"></view>
+					<view :class="{ 'triangle-down-active': fieldValue == 1 }"></view>
+				</block>
+				<block wx:else>
+					<view class="triangle-down"></view>
+				</block>
+			</view>
+			<view class="sort-item flex-item-1 flex-box flex-v-center flex-h-center">
+				<view
+					class="list-mode flex-box flex-v-center flex-h-center flex-item-1"
+					@click="switchListMode(listMode)"
+				>
+					<view :class="['iconfont', listMode == 1 ? 'icon-liebiaomoshi' : 'icon-zhongtumoshi']"></view>
+				</view>
+				<view class="flex-box flex-v-center flex-h-center flex-item-1" @click="showDrawer">
+					筛选
+				</view>
+			</view>
+		</view>
+		<!-- 商品列表 -->
+		<view class="goods-list flex-box flex-wrap flex-h-between">
+			<block v-if="listMode == 1">
+				<view v-for="goodsItem in 15" :key="goodsItem" class="goods-item-1" @click="navigateTo(goodsItem)">{{
+					goodsItem
+				}}</view>
+			</block>
+			<block v-else-if="listMode == 2">
+				<view v-for="goodsItem in 15" :key="goodsItem" class="goods-item-2" @click="navigateTo(goodsItem)">{{
+					goodsItem
+				}}</view>
+			</block>
+		</view>
+		<!-- 筛选抽屉 -->
+		<uni-drawer ref="showRight" mode="right">
+			<scroll-view style="height: 100%;" scroll-y="true">
+				<button @click="closeDrawer" type="primary">关闭Drawer</button>
+				<view v-for="item in 60" :key="item">可滚动内容 {{ item }}</view>
+			</scroll-view>
+		</uni-drawer>
 	</view>
 </template>
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			words: '',
+			sortList: [
+				{ prop: 'sales', label: '销量' },
+				{ prop: 'date', label: '上市时间' },
+				{ prop: 'price', label: '价格' }
+			],
+			currentField: 'sales', // 当前排序字段
+			fieldValue: 1, // 当前排序值 1：降序   2： 升序
+			listMode: 1, // 列表模式   1: 单列   2：双列
+			goodsList: []
+		};
 	},
 	methods: {
-		navigateTo() {
-			this.pushUrl('/pages/goods/detail');
+		// 页面跳转
+		navigateTo(sn) {
+			this.pushUrl(`/pages/goods/detail?sn=${sn}`);
+		},
+		// tab栏切换
+		changeSortField(field, type) {
+			this.batchUpdate({
+				currentField: field,
+				fieldValue: type == 1 ? 2 : 1
+			});
+		},
+
+		// 切换列表模式
+		switchListMode(mode) {
+			this.batchUpdate({
+				listMode: mode == 1 ? 2 : 1
+			});
+		},
+		showDrawer() {
+			this.$refs.showRight.open();
+		},
+		closeDrawer() {
+			this.$refs.showRight.close();
 		}
 	}
 };
 </script>
+<style lang="scss">
+.sort-box {
+	padding: 10rpx 0;
+	font-size: 26rpx;
+	background-color: #fff;
+	position: sticky;
+	top: 0;
+
+	.item-active {
+		color: #f80707;
+	}
+
+	.triangle-up {
+		width: 0;
+		height: 0;
+		border-left: 4px solid transparent;
+		border-right: 4px solid transparent;
+		border-bottom: 6px solid #aaa;
+	}
+
+	.triangle-up-active {
+		width: 0;
+		height: 0;
+		border-left: 4px solid transparent;
+		border-right: 4px solid transparent;
+		border-bottom: 6px solid #f80707;
+	}
+
+	.triangle-down {
+		width: 0;
+		height: 0;
+		border-left: 4px solid transparent;
+		border-right: 4px solid transparent;
+		border-top: 6px solid #aaa;
+	}
+
+	.triangle-down-active {
+		width: 0;
+		height: 0;
+		border-left: 4px solid transparent;
+		border-right: 4px solid transparent;
+		border-top: 6px solid #f80707;
+	}
+
+	.sort-item {
+		.list-img,
+		.filter-img {
+			width: 48rpx;
+			height: 48rpx;
+		}
+	}
+
+	.list-mode {
+		position: relative;
+	}
+
+	.list-mode::after {
+		position: absolute;
+		content: '';
+		display: flex;
+		width: 2rpx;
+		height: 30rpx;
+		background-color: #888;
+		top: 50%;
+		right: 0;
+		transform: translateY(-50%);
+	}
+}
+
+.goods-list {
+	padding: 20rpx 32rpx 0;
+	.goods-item-1 {
+		width: 100%;
+		height: 200rpx;
+		background-color: #fff;
+		margin-bottom: 20rpx;
+		border-radius: 10rpx;
+	}
+
+	.goods-item-2 {
+		width: 48%;
+		height: 200rpx;
+		background-color: #fff;
+		margin-bottom: 20rpx;
+		border-radius: 10rpx;
+	}
+}
+.icon-liebiaomoshi {
+	font-size: 32rpx;
+	color: #000;
+}
+</style>
