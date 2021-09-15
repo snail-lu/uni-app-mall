125d7fe091f1235f42ad3cad657e0104db845f90
snaillu <snaillu@yeah.net>
2021-09-15 14:01:46 +0800

    创建订单页，页面优化

--- src/pages/order/create.vue
+++ src/pages/order/create.vue
@@ -1,86 +1,65 @@
 <template>
-	<view class="order-box">
+	<view class="create-order-container">
 		<!-- 有地址 -->
-		<view class="address-item" v-if="address.id">
-			<view class="address-cent flex-box flex-v-center flex-h-between">
-				<view class="address-left">
-					<view class="address-top flex-box flex-v-center flex-h-between">
-						<view class="address-name">{{ address.recevier }}</view>
-						<view class="address-mobile">{{ address.recevierPhoneNumber }}</view>
-					</view>
-					<view class="address-bot">{{ address.recevierAddress }}</view>
+		<view v-if="address.id" class="address-item flex-box" @click="chooseAddress">
+			<view class="flex-item-1">
+				<view class="flex-box">
+					<view class="username">{{ address.consignee }}</view>
+					<view class="mobile">{{ address.mobile }}</view>
+					<view class="default" v-if="address.isDefault">默认</view>
 				</view>
-				<image
-					src="/static/checkbox/right.png"
-					class="right-jian"
-					style="margin-left:20rpx"
-					@click="goAddress"
-				></image>
+				<view class="address-text">
+					{{ `${address.province}${address.city}${address.district}${address.detailAddress}` }}</view
+				>
+			</view>
+			<view class="flex-box-v flex-v-center">
+				<uni-icons type="arrowright" size="20" color="#666" />
 			</view>
 		</view>
 		<!-- 地址无 -->
-		<view class="address-item" v-else>
-			<view class="address-k">
-				<view class="address-k-text" style="font-size:26rpx;color:#666">暂无地址</view>
-				<view class="address-k-bot flex-box flex-v-center" @click="goAddress"
-					><span>请新建收货地址</span><image src="/static/checkbox/right.png" class="right-jian"></image
-				></view>
-			</view>
+		<view v-else class="address-item flex-box flex-v-center flex-h-between" @click="chooseAddress">
+			<view class="flex-item-1 empty-address-tips"> 暂无地址，去新建 </view>
+			<uni-icons type="arrowright" size="20" color="#666" />
 		</view>
 		<!-- 订单商品 -->
-		<view class="goodsList">
-			<block v-for="item in cartGoods.goodsList" :key="item.id">
+		<view class="goods-list">
+			<block v-for="item in cartGoodsList" :key="item.id">
 				<view class="cart-goodsitem flex-box flex-v-center flex-h-between">
-					<image :src="imgIp + item.goodsImgAddress" class="goodsimg"></image>
+					<image :src="imgIp + item.goodsImgAddress" class="goods-img"></image>
 					<view class="goods-right">
 						<view class="goodsname over-dian">{{ item.goodsName }}</view>
 						<view class="goods-cent flex-box flex-h-between">
-							<view class="sizename over-dian">尺寸：{{ item.goodsSize }}</view>
-							<view class="codename over-dian">颜色：{{ item.goodsColor }}</view>
+							<view class="sizename over-dian">{{ item.goodsSpec }}</view>
 						</view>
 						<view class="goods-bot flex-box flex-h-between">
-							<view class="goods-price">￥{{ item.goodsPrice }}</view>
+							<view class="goods-price">￥{{ item.salePrice }}</view>
 							<view class="count flex-box flex-v-center">
-								<span class="input-num">×{{ item.goodsQuantity }}</span>
+								<view class="goods-num">×{{ item.goodsNum }}</view>
 							</view>
 						</view>
 					</view>
 				</view>
-				<view class="outbox">
-					<uni-collapse id="alicoll" v-model="value">
-						<uni-collapse-item name="key1" title="定制详情">
-							<view class="detail-box flex-box flex-v-center" style="flex-wrap:wrap;">
-								<view
-									class="detail-item"
-									style="width:50%"
-									v-for="DotItem in item.goodsPartSubDtoOtherList"
-									:key="DotItem.id"
-									>{{ DotItem.goodsPartName }}:{{ DotItem.goodsPartsDescribe }}</view
-								>
-							</view>
-						</uni-collapse-item>
-					</uni-collapse>
-				</view>
 			</block>
-			<!-- <view class="redtip">注：订单支付成功之后不支持取消订单</view> -->
 		</view>
 		<!-- 订单结算 -->
 		<view class="order-bot">
 			<view class="title" style="margin:20rpx 0 40rpx 0;">价格明细</view>
 			<view class="flex-box flex-v-center flex-h-between">
 				<span>商品总价</span>
-				<span>￥{{ goodsAmount }}</span>
+				<span>￥{{ orderInfo.goodsAmount }}</span>
 			</view>
-			<view class="flex-box color-666 flex-v-center flex-h-between" v-if="freightAmount">
+			<view class="flex-box color-666 flex-v-center flex-h-between">
 				<span>运费</span>
-				<span>￥{{ freightAmount }}</span>
+				<span>￥{{ orderInfo.freightAmount }}</span>
 			</view>
 			<view class="flex-box color-666 flex-v-center flex-h-between">
 				<span>优惠</span>
 				<!-- <picker @change="bindPickerChange" mode="selector" :value="index" :range="couponList" range-key='voucherName'> -->
 				<view class="flex-box color-666 flex-v-center flex-h-between" @click="showPopup">
-					<span>{{ reducePrice ? '-' + reducePrice : Nullcoupon ? '暂无优惠券可用' : '不使用优惠券' }}</span
-					><image src="/static/checkbox/right.png" class="rightimg"></image>
+					<span>{{
+						orderInfo.discount ? '-' + orderInfo.discount : Nullcoupon ? '暂无优惠券可用' : '不使用优惠券'
+					}}</span
+					><uni-icons type="arrowright" size="20"></uni-icons>
 				</view>
 				<!-- </picker> -->
 			</view>
@@ -152,54 +131,96 @@
 		<!-- 底部付款 -->
 		<view class="pay-box flex-box flex-v-center flex-h-between">
 			<view class="pay-left">
-				<view class="pay-top color-666" style="font-size:24rpx">共计{{ cartGoods.goodsList.length }}件</view>
-				<view class="pay-bottom">合计￥{{ orderPaidAmount }}</view>
+				<view class="pay-top color-666" style="font-size:24rpx">共计{{ orderInfo.totalGoods }}件</view>
+				<view class="pay-bottom">合计￥{{ orderInfo.orderPaidAmount }}</view>
 			</view>
-			<button class="alibutton btn-width" :disabled="aliDisabled" @click="addorder">立即付款</button>
+			<button class="alibutton btn-width" :disabled="aliDisabled" @click="clickToPay">去支付</button>
 		</view>
 	</view>
 </template>
 <script>
-import PlatformService from '@/services/common/platformService.js';
+import pageUrl from '@/config/page';
 import { mapState } from 'vuex';
 export default {
 	data() {
 		return {
-			address: {
-				// name:'czl',
-				// mobile:'19825215151',
-				// address:'公司官方建安施工发稿时间发噶几十个发健康公司法施工法官法借古讽今案高发'
+			orderInfo: {
+				goodsAmount: '1599.00', // 商品总价
+				discount: '200.00', // 优惠价格
+				totalGoods: 3, // 商品数量
+				orderPaidAmount: '1399.00', // 支付金额
+				freightAmount: 0 // 运费
 			},
-			cartGoods: [],
-			reducePrice: 0,
+			defaultAddress: {
+				id: 1,
+				consignee: '路先生',
+				mobile: '13112341234',
+				isDefault: 1,
+				province: '上海市',
+				city: '上海市',
+				district: '浦东新区',
+				detailAddress: '世纪大道2001号'
+			},
+			cartGoodsList: [
+				{
+					id: 1,
+					goodsName: '测试商品1，名字超长测试测试测试测试测试',
+					marketPrice: '599.00',
+					salePrice: '342.00',
+					goodsNum: 1,
+					goodsSpec: '黑色，XXL'
+				},
+				{
+					id: 2,
+					goodsName: '测试商品2，名字超长测试测试测试测试测试',
+					marketPrice: '499.00',
+					salePrice: '299.00',
+					goodsNum: 1,
+					goodsSpec: '藏青色，XXL'
+				},
+				{
+					id: 3,
+					goodsName: '测试商品3，名字超长测试测试测试测试测试',
+					marketPrice: '459.00',
+					salePrice: '399.00',
+					goodsNum: 1,
+					goodsSpec: '灰色，XXL'
+				},
+				{
+					id: 4,
+					goodsName: '测试商品4，名字超长测试测试测试测试测试',
+					marketPrice: '359.00',
+					salePrice: '299.00',
+					goodsNum: 1,
+					goodsSpec: '白色，XXL'
+				}
+			],
 			selectedCoupon: '',
 			Nullcoupon: true,
 			voucherSubId: '',
 			FromOrderDetail: {},
 			freightAmount: '',
 			imgIp: '',
-			goodsAmount: '',
-			orderPaidAmount: '',
+
 			couponList: [],
 			aliDisabled: false,
 			fromPage: 'cart',
 			couponForm: {
-				memberId: PlatformService.getStorageSync('userInfo').id,
+				memberId: '',
 				voucherStatus: 1
 			}
 		};
 	},
 	computed: {
-		...mapState({
-			cartNum: state => state.cartNum
-		})
+		...mapState({ selectAddress: state => state.address }),
+		address() {
+			return this.selectAddress || this.defaultAddress;
+		}
 	},
 	methods: {
-		navigateTo() {
-			PlatformService.pushUrl('/pages/order/payComplete');
-		},
-		goAddress() {
-			PlatformService.pushUrl('/pages/address/index?page=order');
+		// 选择地址
+		chooseAddress() {
+			this.pushUrl('/pages/address/index?page=order');
 		},
 		// 显示弹窗
 		showPopup() {
@@ -217,8 +238,19 @@ export default {
 		hidePopup() {
 			this.$refs.popup.close();
 		},
+
+		// 确认弹窗
 		onConfirmCoupon() {
 			this.hidePopup();
+		},
+
+		// 支付
+		clickToPay() {
+			if (this.address && this.address.id) {
+				this.pushUrl(pageUrl.payResult);
+			} else {
+				this.tips('收货地址不可为空');
+			}
 		}
 	},
 	onShow() {
@@ -243,6 +275,144 @@ page {
 	height: calc(100% - 240rpx);
 	background-color: #f8f8f8;
 }
+.create-order-container {
+	padding: 0;
+	.address-item {
+		padding: 32rpx 26rpx;
+		background-color: #fff;
+		border-radius: 10rpx;
+		margin-bottom: 20rpx;
+
+		.edit-icon {
+			margin-top: 22rpx;
+			width: 100rpx;
+			height: 32rpx;
+			padding: 6rpx;
+		}
+		.username,
+		.mobile {
+			font-size: 28rpx;
+			font-weight: 500;
+			color: #333;
+		}
+		.username {
+			padding-right: 52rpx;
+		}
+		.mobile {
+			padding-right: 20rpx;
+		}
+		.default {
+			height: 30rpx;
+			line-height: 30rpx;
+			padding: 0 6rpx;
+			border-radius: 6rpx;
+			background-color: #ff4554;
+			color: #fff;
+			font-size: 20rpx;
+			margin: 4rpx;
+		}
+		.address-text {
+			margin-top: 28rpx;
+			font-size: 22rpx;
+			line-height: 22rpx;
+			color: #4f5052;
+			white-space: normal;
+			word-break: break-all;
+			line-height: 1.3;
+		}
+
+		.empty-address-tips {
+			font-size: 28rpx;
+			padding: 10rpx 0;
+		}
+	}
+	.goods-list {
+		margin-top: 30rpx;
+		padding: 20rpx 30rpx;
+		background: #fff;
+		.redtip {
+			padding: 20rpx 20rpx 40rpx 20rpx;
+			font-size: 28rpx;
+			color: #e82a2a;
+		}
+		.cart-goodsitem {
+			height: 216rpx;
+			background: #fff;
+			border-bottom: solid #eee 1rpx;
+			.checkimg {
+				width: 35rpx;
+				height: 35rpx;
+			}
+			.goods-img {
+				width: 160rpx;
+				height: 160rpx;
+				border-radius: 10rpx;
+				overflow: hidden;
+				background-color: #eee;
+			}
+			.goods-right {
+				width: 500rpx;
+			}
+			.outleft {
+				padding: 10rpx 20rpx;
+				font-size: 26rpx;
+				border-radius: 8rpx;
+				background: #959697;
+				color: #fff;
+			}
+			.goodsbot {
+				font-size: 28rpx;
+				color: #959697;
+				margin-top: 48rpx;
+			}
+			.goodsname {
+				width: 460rpx;
+				font-size: 26rpx;
+				margin-bottom: 10rpx;
+			}
+			.sizename {
+				width: 119rpx;
+				text-align: center;
+				padding: 6rpx 14rpx;
+				line-height: 32rpx;
+				background: #f5f5f5;
+				font-size: 12px;
+				border-radius: 16rpx;
+				color: #666;
+			}
+			.codename {
+				width: 130rpx;
+				text-align: center;
+				padding: 6rpx 14rpx;
+				line-height: 32rpx;
+				color: #666;
+				background: #f5f5f5;
+				font-size: 12px;
+				border-radius: 16rpx;
+			}
+			.goods-bot {
+				margin-top: 40rpx;
+				.count {
+					overflow: hidden;
+					margin: 0 16px 0 -20px;
+					.goods-num {
+						display: inline-block;
+						line-height: 38rpx;
+						text-align: center;
+						font-size: 26rpx;
+						cursor: pointer;
+						width: 60rpx;
+						height: 38rpx;
+						color: #333;
+						border-left: 0;
+						border-right: 0;
+					}
+				}
+			}
+		}
+	}
+}
+
 .add-check {
 	width: 42rpx;
 	height: 42rpx;
@@ -295,7 +465,7 @@ page {
 	bottom: 0;
 	padding: 0 30rpx;
 	width: 690rpx;
-	height: 200rpx;
+	height: 120rpx;
 	background: #ffffff;
 	box-shadow: inset 0px 2px 0px 0px rgba(238, 238, 238, 1);
 	.pay-bottom {
@@ -306,14 +476,14 @@ page {
 		border: none;
 		justify-content: center;
 		width: 360rpx;
-		height: 100rpx;
-		line-height: 100rpx;
+		height: 80rpx;
+		line-height: 80rpx;
 		margin: 0;
 		text-align: center;
 		background: #000000;
 		border-radius: 50rpx;
 		color: #fff;
-		font-size: 36rpx;
+		font-size: 28rpx;
 		margin-left: 40rpx;
 	}
 }
@@ -353,129 +523,11 @@ page {
 .color-666 {
 	color: #666;
 }
-#alicoll .uni-collapse-cell.uni-collapse-cell--open {
-	background-color: #fff;
-}
-#alicoll .uni-collapse-cell {
-	border-color: #fff;
-}
-#alicoll .uni-collapse-cell__title {
-	background-color: #fff;
-	padding: 0;
-}
 .outbox {
 	width: 690rpx;
 	margin: 0 auto;
 	background: #fff;
 	position: relative;
 	overflow: hidden;
-}
-.order-box {
-	padding: 0;
-	.address-item {
-		padding: 40rpx 30rpx;
-		background: #fff;
-	}
-	.address-cent .address-left {
-		flex: 1;
-		.address-top {
-			.address-name {
-				color: #000;
-				font-size: 32rpx;
-			}
-			.address-mobile {
-				color: #666;
-				font-size: 28rpx;
-			}
-		}
-		.address-bot {
-			font-size: 26rpx;
-			line-height: 44rpx;
-			margin-top: 20rpx;
-		}
-	}
-	.goodsList {
-		margin-top: 30rpx;
-		padding: 20rpx 30rpx;
-		background: #fff;
-		.redtip {
-			padding: 20rpx 20rpx 40rpx 20rpx;
-			font-size: 28rpx;
-			color: #e82a2a;
-		}
-		.cart-goodsitem {
-			height: 216rpx;
-			background: #fff;
-			border-bottom: solid #eee 1rpx;
-			.checkimg {
-				width: 35rpx;
-				height: 35rpx;
-			}
-			.goodsimg {
-				width: 160rpx;
-				height: 160rpx;
-				border-radius: 10rpx;
-				overflow: hidden;
-			}
-			.goods-right {
-				width: 460rpx;
-			}
-			.outleft {
-				padding: 10rpx 20rpx;
-				font-size: 26rpx;
-				border-radius: 8rpx;
-				background: #959697;
-				color: #fff;
-			}
-			.goodsbot {
-				font-size: 28rpx;
-				color: #959697;
-				margin-top: 48rpx;
-			}
-			.goodsname {
-				width: 460rpx;
-				margin-bottom: 10rpx;
-			}
-			.sizename {
-				width: 119rpx;
-				text-align: center;
-				padding: 6rpx 14rpx;
-				line-height: 32rpx;
-				background: #f5f5f5;
-				font-size: 12px;
-				border-radius: 16rpx;
-				color: #666;
-			}
-			.codename {
-				width: 130rpx;
-				text-align: center;
-				padding: 6rpx 14rpx;
-				line-height: 32rpx;
-				color: #666;
-				background: #f5f5f5;
-				font-size: 12px;
-				border-radius: 16rpx;
-			}
-			.goods-bot {
-				margin-top: 40rpx;
-				.count {
-					overflow: hidden;
-					margin: 0 16px 0 -20px;
-					.input-num {
-						display: inline-block;
-						line-height: 38rpx;
-						text-align: center;
-						font-size: 16px;
-						cursor: pointer;
-						width: 60rpx;
-						height: 38rpx;
-						color: #333;
-						border-left: 0;
-						border-right: 0;
-					}
-				}
-			}
-		}
-	}
 }
 </style>
