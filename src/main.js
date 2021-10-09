8b5687c7b2853e95b30786dbf35b4653bb00c0d3
snaillu <snaillu@yeah.net>
2021-10-09 14:50:09 +0800

    公用样式提取至文件common.scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App';
 import store from './store';
 import commonMixins from '@/common/mixins/commonMixins';
 import './static/iconfont/iconfont.css';
+import './static/styles/common.scss';
 
 Vue.config.productionTip = false;
 Vue.prototype.$store = store;
